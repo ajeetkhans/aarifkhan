d9025ec1f1e25c33fcf65c76606fd3621d91b926
ajitkhans <ajeetkhan0420@gmail.com>
2023-02-21 23:15:39 +0530

    test

--- index.js
+++ index.js
@@ -1,52 +1,124 @@
 
 
+//  {{{{{{{    1    }}}}}}}
+   function capitalizeFirstLetter(str) {
 
-// \////////////////////////////////////////////////////////////////\\\////\\/\\/\\\\
+    
+    const capitalized = str.charAt(0).toUpperCase() + str.slice(1);
+
+    return capitalized;
+}
 
 
-///////// { 1 }
-// let fries = Number (prompt("How many french"))
-// let burgers = Number (prompt("How many "))
-// let chowmin =Number(prompt("How many plates"))
-// let  Manchurian =Number(prompt("How many plates of "))
-// let  Cokes =Number(prompt("How many"))
-// console.log(`fries${name}
-// burgers${name}
-// chowmin${name}
-// Manchurian${name}
-// Cokes${name}`);
-// let total=fries+burgers+chowmin+Manchurian+Cokes
+const string = prompt('wecodeacademy ');
 
-///\\\/\/\/\\\\\\\\/\/\\\\/\\/\//////////////////\/\/\/\/\/\/\/\/\/\//////\/\\/\/\/\/\
+const result = capitalizeFirstLetter(string);
 
-// for(let a =2;a<=3130;a*=3){
-//     console.log(a);
-// }
-// \\\\\\\\\\\\\\\\\\\\\\////\//\\/\/\////////////////////////////////////////
+console.log(result);
 
-///////{3}
+// {{{ 2 }}}
 
-// let number=prompt("neinter is numer ");
-// if (number===5){
-//     console.log(`${number}is neinter`);
-// }else if (number<=1){
-//     console.log(`${number}is not `);
-// }{
-//     for(let a= 1;a<=number;a++){
-//         if(Number%a==0){
-//             let res=(`${number}is not a numer`);
-//         }else{
-//             let res=(`${number}is yes  a numer`);
-//         }
-//     }
-//     console.log("res");
-// }
+insert = function insert(main_string, ins_string, pos) {
+    if(typeof(pos) == "undefined") {
+     pos = 0;
+   }
+    if(typeof(ins_string) == "undefined") {
+     ins_string = '';
+   }
+    return main_string.slice(0, pos) + ins_string + main_string.slice(pos);
+     }
+ console.log(insert('We are doing some exercises.'));
+ console.log(insert('We are doing some exercises.','JavaScript '));
+ console.log(insert('We are doing some exercises.','JavaScript ',14));
 
-//\\\//\/\/\////////////////////////////////////
-// let num = 121
-// let originga=num;
-// let reverse =0;
-// while(num!=0){
-//     reverse(reverse*10)+(num%10)
-//     num=parseInt(num/0) 
-// }
+
+
+//  {{{{{  3  }}}}}
+
+function count(main_str, sub_str) 
+    {
+    main_str += '';
+    sub_str += '';
+
+    if (sub_str.length <= 0) 
+    {
+        return main_str.length + 1;
+    }
+
+       subStr = sub_str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+       return (main_str.match(new RegExp(subStr, 'gi')) || []).length;
+    }
+ 
+console.log(count("The quick brown fox jumps over the lazy dog", 'the'));
+console.log(count("The quick brown fox jumps over the lazy dog", 'fox',false));
+// {{{{  4  }}}}
+
+let aji ="8875492"
+console.log(aji.padEnd(10,"*"));
+let jj=""
+
+let addr = "9573663527329";
+let index = addr.indexOf("8875767");
+let rightSide = addr.slice(index);
+let leftside = addr.slice(0, index);
+let finalHalf = leftside.slice(0, leftside.length / 2);
+let masked = finalHalf.padEnd(leftside.length, "*");
+console.log(masked + rightSide);
+
+let atm ="9876434"
+console.log(atm.padEnd(15,"*"));
+
+// {{{{{    5    }}}}}
+
+let p = 5;
+let st = "";
+
+for (let i = 1; i <= p; i++) {
+ 
+  for (let j = 1; j <= p - i; j++) {
+    st += " ";
+  }
+  
+  for (let k = 0; k < 2 * i - 1; k++) {
+    st += "$";
+  }
+  st += "\n";
+}
+console.log(st);
+
+// {{{{{  6 }}}}}
+let b = prompt("what character is present at particular index in a word.")
+let a = "wecode academy";
+if(a ){
+    console.log("Congrulations")
+ }else{
+    console.log(NO)
+}
+
+
+// {{{{  7 }}}}
+let str = '';
+let substrings = str.concat('Wecode ', 'Hello ', 'World ', 'Academy');
+
+console.log(substrings);
+
+// {{{{{{{{   9    }}}}}}}}
+
+let n = 5;
+let strin = "";
+
+for (let i = 5; i >= 1; i--) {
+ 
+  for (let j = 1; j <= n - i; j++) {
+    strin += " ";
+  }
+
+  for (let k = 0; k < 2 * i - 1; k++) {
+    strin += "*";
+  }
+  strin += "\n";
+}
+console.log(strin);
+
+
+
--- inxex-1.md
+++ inxex-1.md
@@ -49,3 +49,29 @@ console.log(`fries${name}
      num=parseInt(num/0) 
      
  ```
+======================================
+How many french fries (60rs per piece) do you want to order ?
+How many burgers (50rs per piece) do you want to order ?
+How many plates of chowmin (100rs per plate) do you want to order?
+How many plates of Manchurian (80rs per plate) do you want to order?
+
+=======================================================
+Using a switch case to do this exercise
+
+If number is odd then print Odd
+If number is even then print Even
+If number is 100 then print 100
+Else print 10000
+===================================================================
+<!-- Ask user his/her birth year, "What is your birth year?"
+
+If user age is greater than 18 then ask user "Do you have valid license number?" If user click on Okay then ask for his license number and finally in the alert show message "User with license number #### is ready to drive."
+
+If user age is less than 18 then ask him question in confirm box "Do you have gaurdians ?"" If user says yes then ask his/her Gaurdian name and then in alert show "You are ready to drive with your #### (show gaurdian name here.)" Else show "Sorry visit again please." in alert box
+ -->
+<!-- Take any number and take other number as 19. Now check their difference by subtracting each other. If both numbers difference is greater than 19 then print triple their absolute difference else print double their absolute difference. -->
+
+take any number and take other nyumber a 19 . now check their differnce by subtracting each other if both number differnce is greater than 19 then print 
+
+
+  11 22 33 44
